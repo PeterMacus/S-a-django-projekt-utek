1433405ecd557c341d1557a95446e7bd9d456d88
skutas23 <skutas23@Z00P1-04>
2026-06-03 10:08:51 +0200

    b

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+if [ -n "$admin" ] && [ -n "$admin" ]; then
+    python manage.py createsuperuser --no-input || true
+fi
